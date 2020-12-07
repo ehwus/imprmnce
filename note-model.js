4113b7a7e93fe84aff65bece324c445593f17714
Alex Withington-Smith <alex@awithsmith.com>
2020-12-07 14:43:21 +0000

    redefined the note

---
+++ note-model.js
@@ -0,0 +1,17 @@
+// In here, I will re-define the note.
+// What is a Note?
+// Since time immemorial, man has noted things
+// We can look at notings from history
+// BUT SHOULD WE BE ABLE TO?
+// What if note but self destroy?
+// like in tom cruise film?
+
+class Note {
+    constructor(text) {
+        this.text = text
+    }
+
+    readNote = function() {
+        return this.text;
+    }
+}
