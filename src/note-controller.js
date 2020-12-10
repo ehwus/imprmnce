8f95ec13c97576bc1c6c7376aa4e5d7a9142f517
Alex Withington-Smith <alex@awithsmith.com>
2020-12-10 07:44:27 +0000

    list view and single note view now tested and working on index

--- src/note-controller.js
+++ src/note-controller.js
@@ -1,21 +1,28 @@
 class NoteController {
-    constructor(noteListView, singleViewClass = SingleNoteView) {
-        this.noteListView = noteListView;
-        this.singleViewClass = singleViewClass;
-    }
+  constructor(noteListView, singleViewClass = SingleNoteView) {
+    this.noteListView = noteListView;
+    this.singleViewClass = singleViewClass;
+  }
 
-    insertListHTML(element = document.getElementById('app')) {
-        element.innerHTML += this.noteListView.getHTML();
-    }
+  insertListHTML(element = document.getElementById("app")) {
+    element.innerHTML += this.noteListView.getHTML();
+  }
 
-    insertNoteHTML(noteID, element = document.getElementById('app')) {
-        for (let note of this.noteListView.list.notes) {
-            if (noteID === note.id) {
-                let view = new this.singleViewClass(note);
-                element.innerHTML = view.getHTML();
-                return;
-            }
-        }
-        throw new Error("Note not found");
-    }
-}
+  insertNoteHTML(noteID, element = document.getElementById("app")) {
+    for (let note of this.noteListView.list.notes) {
+      if (noteID === note.id) {
+        let view = new this.singleViewClass(note);
+        element.innerHTML = view.getHTML();
+        return;
+      };
+    };
+    throw new Error("Note not found");
+  };
+};
+
+let list = new NoteList;
+list.addNote("Hello World");
+list.addNote("This is a test");
+
+let listView = new noteListView(list);
+let controller = new NoteController(listView);
