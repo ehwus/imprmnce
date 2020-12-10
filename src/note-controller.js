9c5ffd0ab96df0c526bad019e14f8ff292a6e1a3
Alex Withington-Smith <alex@awithsmith.com>
2020-12-10 07:38:07 +0000

    added individual note view to the controller

--- src/note-controller.js
+++ src/note-controller.js
@@ -1,9 +1,21 @@
 class NoteController {
-    constructor(noteListView) {
+    constructor(noteListView, singleViewClass = SingleNoteView) {
         this.noteListView = noteListView;
+        this.singleViewClass = singleViewClass;
     }
 
-    insertHTML(element) {
+    insertListHTML(element = document.getElementById('app')) {
         element.innerHTML += this.noteListView.getHTML();
+    }
+
+    insertNoteHTML(noteID, element = document.getElementById('app')) {
+        for (let note of this.noteListView.list.notes) {
+            if (noteID === note.id) {
+                let view = new this.singleViewClass(note);
+                element.innerHTML = view.getHTML();
+                return;
+            }
+        }
+        throw new Error("Note not found");
     }
 }
--- tests/spec/noteControllerSpec.js
+++ tests/spec/noteControllerSpec.js
@@ -1,22 +1,55 @@
 describe("NoteController", () => {
-    it("Initialises with a noteListView", () => {
-        let fakeNoteListView = { }
-        let fakeElement;
-        let controller = new NoteController(fakeNoteListView, fakeElement);
-        isSameType(controller.noteListView, fakeNoteListView)
-    });
+  it("Initialises with a noteListView", () => {
+    let fakeNoteListView = {};
+    let fakeElement;
+    let controller = new NoteController(fakeNoteListView, fakeElement);
+    isSameType(controller.noteListView, fakeNoteListView);
+  });
 
-    it("inserts HTML into given element", () => {
-        let fakeNoteListView = {
-            getHTML: () => {
-                return `<h1>TestHTML</h1>`;
-            }
-        }
-        let fakeElement = {
-            innerHTML: ""
-        }
-        let controller = new NoteController(fakeNoteListView, fakeElement);
-        controller.insertHTML(fakeElement)
-        isEqual(fakeElement.innerHTML, '<h1>TestHTML</h1>')
-    });
-})
+  it("inserts noteList HTML into given element", () => {
+    let fakeNoteListView = {
+      getHTML: () => {
+        return `<h1>TestHTML</h1>`;
+      },
+    };
+    let fakeElement = {
+      innerHTML: "",
+    };
+    let controller = new NoteController(fakeNoteListView, fakeElement);
+    controller.insertListHTML(fakeElement);
+    isEqual(fakeElement.innerHTML, "<h1>TestHTML</h1>");
+  });
+
+  it("inserts HTML of element with correct ID", () => {
+    let fakeNoteListView = {
+      list: {
+        notes: [
+          {
+            id: 1,
+            readNote: () => {
+              return "Hello World";
+            },
+          },
+        ],
+      },
+    };
+
+    let fakeElement = {
+      innerHTML: "",
+    };
+
+    class fakeSingleView {
+      constructor(note) {
+        this.note = note;
+      }
+
+      getHTML() {
+        return `<div>${this.note.readNote()}</div>`;
+      }
+    }
+
+    let controller = new NoteController(fakeNoteListView, fakeSingleView);
+    controller.insertNoteHTML(1, fakeElement);
+    isEqual(fakeElement.innerHTML, "<div>Hello World</div>");
+  });
+});
